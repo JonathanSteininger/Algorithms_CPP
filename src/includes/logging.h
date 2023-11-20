ab8c8a08afccbcd5f2f469e7171037cf807f0681
Jonathan Steininger <jonathan.steininger@icloud.com>
2023-11-20 19:43:06 +1300

    Updated the logging file to more easily manipulate in the code.

--- src/includes/logging.h
+++ src/includes/logging.h
@@ -1,30 +1,77 @@
 #pragma once
 #include <iostream>
 
-static void writeSourceLocation(const char *fileName, int lineNumber) {
+static void writeSourceLocation(const char *fileName, int lineNumber, std::string *_location){
 #ifdef ENABLE_LOGGING
-    std::cout << "file: " << fileName << " @:" << std::to_string(lineNumber)
-              << " | ";
+    static std::vector<std::string> LogList = std::vector<std::string>();
+    static bool loggingEnabled = true;
+    if(LogList.empty()){
+        LogList.push_back("main");
+    }
+    if(_location != nullptr){
+        if(*_location == "___Toggle___"){
+            loggingEnabled = !loggingEnabled;
+        }
+        if(*_location == "___Remove___"){
+            LogList.pop_back();
+            return;
+        }
+        LogList.push_back(*_location);
+        return;
+    }
+    if(!loggingEnabled){
+        return;
+    }
+    int desiredStart = 50;
+    int totalTraveled = 0;
+    for(std::string section : LogList){
+        totalTraveled += section.length() + 1;
+        std::cout << section << ">";
+    }
+    while(totalTraveled < desiredStart){
+        std::cout << " ";
+        totalTraveled++;
+    }
+    //std::cout << " | file: " << fileName << " @:" << std::to_string(lineNumber) << " | ";
+#endif
+}
+inline void LOG_TOGGLE(){
+#ifdef ENABLE_LOGGING
+    std::string toggleString = "___Toggle___";
+    writeSourceLocation("", 0, &toggleString);
+#endif
+}
+inline void LOG_EXIT_SECTION(){
+#ifdef ENABLE_LOGGING
+    std::string removeString = "___Remove___";
+    writeSourceLocation("", 0, &removeString);
 #endif
 }
 
-template <typename T> inline void writeStandardLog(T text) {
+inline void LOG_ADD_SECTION(std::string location){
 #ifdef ENABLE_LOGGING
-    writeSourceLocation(__FILE__, __LINE__);
+    writeSourceLocation("", 0, &location);
+#endif
+}
+template <typename T>
+inline void LOG(T text){
+#ifdef ENABLE_LOGGING
+    writeSourceLocation(__FILE__, __LINE__, nullptr);
     std::cout << text << "\n";
 #endif
 }
-template <typename T>
-inline void writeVariableLog(std::string text, T variable) {
+template <typename T> 
+inline void LOG_VARS(std::string text, T variable){
 #ifdef ENABLE_LOGGING
-    writeSourceLocation(__FILE__, __LINE__);
+    writeSourceLocation(__FILE__, __LINE__, nullptr);
     std::cout << text << std::to_string(variable) << "\n";
 #endif
 }
-template <typename T>
-inline void writeVariableLogSimple(std::string text, T variable) {
+template <typename T> 
+inline void LOG_VARS_SIMPLE(std::string text, T variable){
 #ifdef ENABLE_LOGGING
-    writeSourceLocation(__FILE__, __LINE__);
+    writeSourceLocation(__FILE__, __LINE__, nullptr);
     std::cout << text << variable << "\n";
 #endif
 }
+
