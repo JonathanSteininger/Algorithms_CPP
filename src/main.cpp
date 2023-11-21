026f4661467c4353437caca940dd8cfc2956746a
Jonathan Steininger <jonathan.steininger@icloud.com>
2023-11-21 20:19:49 +1300

    Tests the Get function.

--- src/main.cpp
+++ src/main.cpp
@@ -66,10 +66,14 @@ bool testBalancedTree(){
     std::sort(nums.begin(), nums.end());
     bool flag = true;
     for(int i = 0; i < nums.size(); i++){
-
+        int headValue = head.get(i);
+        LOG_VARS("TreeValue: ", headValue);
+        LOG_VARS("intendedValue: ", nums[i]);
+        if(flag){
+            flag = headValue == nums[i];
+        }
     }
-    return true;
-
+    return flag;
 }
 
 int main(int argc, char **argv) {
