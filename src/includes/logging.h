755272413c678884a8583471e4a4989e4f290243
Jonathan Steininger <jonathan.steininger@icloud.com>
2023-11-22 01:07:55 +1300

    fixed toggeling logging only disabeling section logging.

--- src/includes/logging.h
+++ src/includes/logging.h
@@ -1,8 +1,8 @@
 #ifndef LOGGING_JONO
 #define LOGGING_JONO
 #include <iostream>
-
-static void writeSourceLocation(const char *fileName, int lineNumber, std::string *_location){
+#include <sstream>
+static void writeSourceLocation(std::string message, const char *fileName, int lineNumber, std::string *_location){
 #ifdef ENABLE_LOGGING
     static std::vector<std::string> LogList = std::vector<std::string>();
     static bool loggingEnabled = true;
@@ -12,6 +12,7 @@ static void writeSourceLocation(const char *fileName, int lineNumber, std::strin
     if(_location != nullptr){
         if(*_location == "___Toggle___"){
             loggingEnabled = !loggingEnabled;
+            return;
         }
         if(*_location == "___Remove___"){
             LogList.pop_back();
@@ -33,46 +34,50 @@ static void writeSourceLocation(const char *fileName, int lineNumber, std::strin
         std::cout << " ";
         totalTraveled++;
     }
+    std::cout << message;
     //std::cout << " | file: " << fileName << " @:" << std::to_string(lineNumber) << " | ";
 #endif
 }
 inline void LOG_TOGGLE(){
 #ifdef ENABLE_LOGGING
     std::string toggleString = "___Toggle___";
-    writeSourceLocation("", 0, &toggleString);
+    writeSourceLocation("", "", 0, &toggleString);
 #endif
 }
 inline void LOG_EXIT_SECTION(){
 #ifdef ENABLE_LOGGING
     std::string removeString = "___Remove___";
-    writeSourceLocation("", 0, &removeString);
+    writeSourceLocation("","", 0, &removeString);
 #endif
 }
 
 inline void LOG_ADD_SECTION(std::string location){
 #ifdef ENABLE_LOGGING
-    writeSourceLocation("", 0, &location);
+    writeSourceLocation("", "", 0, &location);
 #endif
 }
 template <typename T>
 inline void LOG(T text){
 #ifdef ENABLE_LOGGING
-    writeSourceLocation(__FILE__, __LINE__, nullptr);
-    std::cout << text << "\n";
+    std::stringstream stream;
+    stream << text << "\n";
+    writeSourceLocation(stream.str(), __FILE__, __LINE__, nullptr);
 #endif
 }
 template <typename T> 
 inline void LOG_VARS(std::string text, T variable){
 #ifdef ENABLE_LOGGING
-    writeSourceLocation(__FILE__, __LINE__, nullptr);
-    std::cout << text << std::to_string(variable) << "\n";
+    std::stringstream stream;
+    stream << text << std::to_string(variable) << "\n";
+    writeSourceLocation(stream.str(), __FILE__, __LINE__, nullptr);
 #endif
 }
 template <typename T> 
 inline void LOG_VARS_SIMPLE(std::string text, T variable){
 #ifdef ENABLE_LOGGING
-    writeSourceLocation(__FILE__, __LINE__, nullptr);
-    std::cout << text << variable << "\n";
+    std::stringstream stream;
+    stream << text << variable << "\n";
+    writeSourceLocation(stream.str(), __FILE__, __LINE__, nullptr);
 #endif
 }
 #endif
